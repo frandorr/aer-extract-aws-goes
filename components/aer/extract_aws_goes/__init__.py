15f099bb217a7c9e59d5907c7e442ec28a5167ec
Francisco Dorr <fran.dorr@gmail.com>
2026-03-26 17:42:10 +0000

    feat(quick-260326): export LUT functions from package __init__.py
    - Add extract_resample_lut and apply_resample_lut to imports and __all__

--- components/aer/extract_aws_goes/__init__.py
+++ components/aer/extract_aws_goes/__init__.py
@@ -1,3 +1,15 @@
-from .core import detect_reader, extract_aws_goes, map_channel_ids_to_satpy_names
+from .core import (
+    detect_reader,
+    extract_aws_goes,
+    map_channel_ids_to_satpy_names,
+    extract_resample_lut,
+    apply_resample_lut,
+)
 
-__all__ = ["detect_reader", "extract_aws_goes", "map_channel_ids_to_satpy_names"]
+__all__ = [
+    "detect_reader",
+    "extract_aws_goes",
+    "map_channel_ids_to_satpy_names",
+    "extract_resample_lut",
+    "apply_resample_lut",
+]
